4ccea5e4e02c010bdfbe5a3cffb8a768fd8ea10c
Jason Weiss <jason.awz2005@icloud.com>
2024-09-16 13:40:25 -0700

    added activities 1.0

--- cogs/activities.py
+++ cogs/activities.py
@@ -13,31 +13,31 @@ class Activities(commands.Cog):
         
     @commands.command()
     async def dig(self, ctx):
-        inventory = await edb.get_user_inventory(ctx.author.id)
+        inventory = await edb.get_user_inventory(ctx, user_id=ctx.author.id)
         if 'shovel' not in inventory:
             await ctx.send("You don't have a shovel. you can buy one with `r shop buy shovel`")
         else:
             coins = random.randint(1, 100)
-            current_bal = await edb.get_user_balance(ctx.author.id)
+            current_bal = await edb.get_user_balance(user_id=ctx.author.id)
             await ctx.send(f"you dug and found {my_emojis.CURRENCY_EMOJI} ***{coins}*** {uc.CURRENCY}! use them wisely :P\n-# you had {my_emojis.CURRENCY_EMOJI} {current_bal[0]} {uc.CURRENCY}")
             await edb.update_balance(ctx.author.id, wallet=coins)
     
     @commands.command()
     async def hunt(self, ctx):
-        inventory = await edb.get_user_inventory(ctx.author.id)
+        inventory = await edb.get_user_inventory(ctx, user_id=ctx.author.id)
         if 'axe' or 'rifle'not in inventory:
             await ctx.send("You don't have an axe/rifle. you can buy one with `r shop buy axe` or `r shop buy rifle`")
         else:
             if 'axe' in inventory:
                 coins = random.randint(1, 100)
-                current_bal = await edb.get_user_balance(ctx.author.id)
+                current_bal = await edb.get_user_balance(user_id=ctx.author.id)
                 await ctx.send(f"you hunted and found {my_emojis.CURRENCY_EMOJI} ***{coins}*** {uc.CURRENCY}! use them wisely :P\n-# you had {my_emojis.CURRENCY_EMOJI} {current_bal[0]} {uc.CURRENCY}")
-                await edb.update_balance(ctx.author.id, wallet=coins)
+                await edb.update_balance(user_id=ctx.author.id, wallet=coins)
             if 'rifle' in inventory:
                 coins = random.randint(50, 200)
-                current_bal = await edb.get_user_balance(ctx.author.id)
+                current_bal = await edb.get_user_balance(user_id=ctx.author.id)
                 await ctx.send(f"you hunted and found {my_emojis.CURRENCY_EMOJI} ***{coins}*** {uc.CURRENCY}! use them wisely :P\n-# you had {my_emojis.CURRENCY_EMOJI} {current_bal[0]} {uc.CURRENCY}")
-                await edb.update_balance(ctx.author.id, wallet=coins)
+                await edb.update_balance(user_id=ctx.author.id, wallet=coins)
             
 
 
